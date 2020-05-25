b9713bb2a80c7a8c0204ec653517a780221a1315
sic-mundus <mrlouboltz@gmail.com>
2020-05-25 09:36:26 +0200

    Test interaction with the map. Implemented pan, zoom and fit to bounds

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,27 +1,33 @@
 <template>
-  <GmapMap
-    ref="mapRef"
-    :options="mapOptions"
-    :center="center"
-    :zoom="7"
-    style="width: 100%; height: 100%"
-  >
-    <gmap-cluster>
-      <GmapMarker
-        :key="index"
-        v-for="(m, index) in markers"
-        :position="m.position"
-        :clickable="true"
-        :draggable="true"
-        @click="center=m.position"
-      />
-    </gmap-cluster>
-  </GmapMap>
+  <div class="wrapper">
+    <v-btn @click="changeCenter">Change Center</v-btn>
+    <v-btn @click="changeZoom">Change Zoom</v-btn>
+    <v-btn @click="fitBounds">Fit to bounds</v-btn>
+    <GmapMap
+      ref="mapRef"
+      :options="mapOptions"
+      :center="center"
+      :zoom="zoom"
+      style="width: 100%; height: 100%"
+    >
+      <gmap-cluster>
+        <GmapMarker
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          :clickable="true"
+          :draggable="true"
+          @click="center=m.position"
+        />
+      </gmap-cluster>
+    </GmapMap>
+  </div>
 </template>
 
 <script>
 import { gmapApi } from "vue2-google-maps";
 export default {
+  m: null,
   data: () => ({
     mapOptions: {
       zoomControl: true,
@@ -37,21 +43,29 @@ export default {
         name: "Pirulino 1",
         position: {
           lat: 42.1,
-          lng: 18
+          lng: 11
         }
       },
       {
         name: "Pirulino 2",
         position: {
-          lat: 60.2,
+          lat: 43.2,
           lng: 10
+        }
+      },
+      {
+        name: "Pirulino 3",
+        position: {
+          lat: 41.2,
+          lng: 1.2
         }
       }
     ],
     center: {
       lat: 42,
       lng: 10
-    }
+    },
+    zoom: 7
   }),
   computed: {
     google: gmapApi
@@ -62,12 +76,11 @@ export default {
     // Therefore we need to write mapRef.$mapPromise.then(() => ...)
 
     this.$refs.mapRef.$mapPromise.then(map => {
-      this.panToBounds(map);
+      this.m = map;
     });
   },
   methods: {
-    panToBounds(map) {
-      //console.log("goole:", this.google.maps.);
+    fitBounds() {
       var b = new this.google.maps.LatLngBounds();
 
       this.markers.forEach(mk => {
@@ -77,8 +90,26 @@ export default {
         });
       });
 
-      map.panToBounds(b);
+      this.m.fitBounds(b);
+    },
+
+    changeCenter() {
+      this.center = {
+        lat: 44 + Math.random() * 0.3,
+        lng: 10 + Math.random() * 0.1
+      };
+    },
+
+    changeZoom() {
+      this.zoom = Math.floor(5 + Math.random() * 10);
     }
   }
 };
 </script>
+
+<style scoped>
+.wrapper {
+  width: 100%;
+  height: 100%;
+}
+</style>
